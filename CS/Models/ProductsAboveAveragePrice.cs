096b64ef85f249fff4687ade74bf3a5b1a0d63ea
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2023-03-20 12:50:55 +0400

    add the example solution

---
+++ CS/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BindToData.Models;
+
+public partial class ProductsAboveAveragePrice {
+    public string ProductName { get; set; } = null!;
+    public decimal? UnitPrice { get; set; }
+}
---
+++ CS/Models/Region.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BindToData.Models;
+
+public partial class Region {
+    public int RegionId { get; set; }
+    public string RegionDescription { get; set; } = null!;
+    public virtual ICollection<Territory> Territories { get; } = new List<Territory>();
+}
